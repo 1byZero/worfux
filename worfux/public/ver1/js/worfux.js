9cfdbf0a8ef999f3fce25b0c6d415e2463ab4d76
1byzero <amitkp66@live.com>
2023-06-18 05:14:41 +0530

    ver1.0.1

--- worfux/public/ver1/js/worfux.js
+++ worfux/public/ver1/js/worfux.js
@@ -1,1 +1,15 @@
 var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "59e6bc24ccda95b279fc4b11ea7811b2e97e52dc21f68ee4b322651e80b3d0249ac6b26ef5219cc050441cf6f4058d59", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
+
+// Google Tag (gtag.js)
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-5QE3JN92TF';
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-5QE3JN92TF');
